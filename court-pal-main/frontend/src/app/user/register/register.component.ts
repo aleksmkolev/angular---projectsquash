561af8c3424228948b424247baf3e25b3880b094
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2024-12-10 22:47:07 +0000

    Fixed imports

--- court-pal-main/frontend/src/app/user/register/register.component.ts
+++ court-pal-main/frontend/src/app/user/register/register.component.ts
@@ -5,11 +5,16 @@ import { emailValidator } from '../../utils/email.validator';
 import { DOMAINS } from '../../constants';
 import { matchPasswordsValidator } from '../../utils/match-passwords.validator';
 import { UserService } from '../user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [RouterLink, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        RouterLink,
+        ReactiveFormsModule
+    ],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
