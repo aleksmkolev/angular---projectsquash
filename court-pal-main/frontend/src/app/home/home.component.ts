a91a4be54d5038b3f232736a5c32e40c21ae8d78
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2024-12-10 21:10:13 +0000

    Add files via upload
    Animation and pipe implementation

--- court-pal-main/frontend/src/app/home/home.component.ts
+++ court-pal-main/frontend/src/app/home/home.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CapitalizePipe } from '../shared/pipes/capitalize.pipe';
 import { ShortenPipe } from '../shared/pipes/shorten.pipe';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [RouterLink, ShortenPipe],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterModule,
+        CapitalizePipe,
+        ShortenPipe
+    ]
 })
-export class HomeComponent {
-  someText = 'This is some long text that needs to be shortened';
+export class HomeComponent implements OnInit {
+    isVisible = false;
+    showFullText = false;
+    
+    ngOnInit() {
+        setTimeout(() => {
+            this.isVisible = true;
+        }, 1000);
+    }
+
+    toggleText() {
+        this.showFullText = !this.showFullText;
+    }
 }
